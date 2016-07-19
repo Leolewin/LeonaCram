b34d86b57ed7510fae3ed620a97ba29db5456f27
leolewin <leolewin@live.cn>
2016-07-19 23:26:05 +0800

    add session/logger/bodyparser to app.js as study

--- app.js
+++ app.js
@@ -27,9 +27,9 @@ app.use('/', routes);
 app.use('/users', users);
 
 //set static files path
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
 
-//app.use('/static', express.use('public'));  //use '/static' as guazai path
+//app.use('/static', express.use(__dirname + '/public'));  //use '/static' as guazai path
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -82,5 +82,54 @@ app.delete('/user', function (req, res) {
 });
 
 
+// define a custom res.message() method
+// which stores messages in the session
+app.response.message = function(msg){
+  // reference `req.session` via the `this.req` reference
+  var sess = this.req.session;
+  // simply add the msg to an array for later
+  sess.messages = sess.messages || [];
+  sess.messages.push(msg);
+  return this;
+};
+
+// log
+if (!module.parent) app.use(logger('dev'));
+
+// session support
+app.use(session({
+  resave: false, // don't save session if unmodified
+  saveUninitialized: false, // don't create session until something stored
+  secret: 'some secret here'
+}));
+
+// parse request bodies (req.body)
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// allow overriding methods in query (?_method=put)
+app.use(methodOverride('_method'));
+
+// expose the "messages" local variable when views are rendered
+app.use(function(req, res, next){
+  var msgs = req.session.messages || [];
+
+  // expose "messages" local variable
+  res.locals.messages = msgs;
+
+  // expose "hasMessages"
+  res.locals.hasMessages = !! msgs.length;
+
+  /* This is equivalent:
+   res.locals({
+     messages: msgs,
+     hasMessages: !! msgs.length
+   });
+  */
+
+  next();
+  // empty or "flush" the messages so they
+  // don't build up
+  req.session.messages = [];
+});
 module.exports = app;
 
